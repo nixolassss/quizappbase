d4054b3af72f325f7377b1df8f5d4caacf0868d8
nixolassss Oliveira <nicolas.m.o@eaportal.org>
2024-06-19 16:57:50 +0000

    adicionada a função de validar a resposta do usuario

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -11,6 +11,7 @@ let pergunta = 1
 
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId =""
 
 botaoTema.addEventListener("click", () => {
     trocarTema( body, botaoTema)
@@ -70,7 +71,7 @@ function montarPergunta(){
                 <secion class="alternativas">
                     <form action="">
                         <label for="alternativa_a" >
-                            <input type="radio" id="alternativa_a" name="alternativa">
+                            <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
                             <div>
                                 <span>A</span>
                                ${alterarSinais(quiz.questions[pergunta-1].options[0])}
@@ -78,7 +79,9 @@ function montarPergunta(){
                         </label>
 
                         <label for="alternativa_b">
-                             <input type="radio" id="alternativa_b" name="alternativa">
+                             <input type="radio" id="alternativa_b" name="alternativa" value="${
+                                alterarSinais(quiz.questions[pergunta-1].options[1])
+                            }">
                             <div>
                                 <span>B</span>
                                 ${
@@ -88,7 +91,9 @@ function montarPergunta(){
                         </label>
 
                         <label for="alternativa_c">
-                            <input type="radio" id="alternativa_c" name="alternativa">
+                            <input type="radio" id="alternativa_c" name="alternativa" value="${
+                                alterarSinais(quiz.questions[pergunta-1]. options[2])
+                            }">
                             <div>
                                 <span>C</span>
                                 ${
@@ -98,7 +103,9 @@ function montarPergunta(){
                         </label>
 
                         <label for="alternativa_d">
-                             <input type="radio" id="alternativa_d" name="alternativa">
+                             <input type="radio" id="alternativa_d" name="alternativa" value=" ${
+                                    alterarSinais(quiz.questions[pergunta-1]. options[3])
+                                }">
                             <div>
                                 <span>D</span>
                                 ${
@@ -120,6 +127,19 @@ function alterarSinais(texto){
 function guardarResposta(evento){
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    if(resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos +1
+    }else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
 }
 
 async function iniciar(){
@@ -127,9 +147,13 @@ async function iniciar(){
     await buscarPerguntas()
     montarPergunta()
 
-    const inputsResposta = document.querySelectorAll(".alternativa input")
+    const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input =>{
         input.addEventListener("click", guardarResposta)
+
+        if(input.value === quiz.questions[pergunta-1].answer){
+            respostaCorretaId = input.id
+        }
     })
 }
 iniciar()

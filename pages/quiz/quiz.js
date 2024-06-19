7af58edf23ba4b27b7e67ab1b703a6484ead1c04
nixolassss Oliveira <nicolas.m.o@eaportal.org>
2024-06-19 17:12:29 +0000

    adicionada a função do botao de proxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -115,7 +115,7 @@ function montarPergunta(){
                         </label>
                         
                     </form>
-                    <button>Enviar</button>
+                    <button>Responder</button>
                 </secion>
     `
 }
@@ -133,6 +133,20 @@ function guardarResposta(evento){
 }
 
 function validarResposta() {
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText ="Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+   
+    
+    if(pergunta === 10){
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+
     if(resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos +1
@@ -140,13 +154,21 @@ function validarResposta() {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta +1
 }
 
-async function iniciar(){
-    alterarAssunto()
-    await buscarPerguntas()
-    montarPergunta()
+function finalizar(){
+    localStorage.setItem("pontos", pontos)
+    window.location.href = "../resultado/resultado.html"
+}
 
+
+function proximaPergunta () {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+function adicionarEventoInputs(){
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input =>{
         input.addEventListener("click", guardarResposta)
@@ -155,5 +177,14 @@ async function iniciar(){
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar(){
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
+
+    
 }
 iniciar()
